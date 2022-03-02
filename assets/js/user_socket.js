d4585daa1a417baef39b2a4fde9fe989464c900d
mkhanooo <mkhan2khan@live.com>
2022-03-02 16:21:47 +0500

    part 3

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -1,21 +1,24 @@
 import { Socket } from "phoenix";
 
 let socket = new Socket("/socket", { params: { token: window.userToken } });
+let roomId = window.roomId;
 
-socket.connect();
+if (roomId) {
+  socket.connect();
 
-let channel = socket.channel("room:general", {});
-channel
-  .join()
-  .receive("ok", (resp) => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", (resp) => {
-    console.log("Unable to join", resp);
-  });
+  let channel = socket.channel(`room:${roomId}`, {});
+  channel
+    .join()
+    .receive("ok", (resp) => {
+      console.log("Joined successfully", resp);
+    })
+    .receive("error", (resp) => {
+      console.log("Unable to join", resp);
+    });
 
-  channel.on("room:general:new_message", (message) => {
+  channel.on(`room:${roomId}:new_message`, (message) => {
     console.log(message);
+    displayMessage(message);
   });
   document.querySelector("#message-form").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -25,15 +28,11 @@ channel
     });
     input.value = "";
   });
-// channel.on("room:general:new_message", (message) => {
-//   console.log(message);
-// });
+  const displayMessage = (msg) => {
+    let template = `
+      <li class="list-group-item"><strong> ${msg.user.username}</strong>:${msg.body}</li>`;
+    document.querySelector("#display").innerHTML += template;
+  };
+}
 
-// document.querySelector("#message-form").addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   let input = e.target.querySelector("#message-body");
-
-//   channel.push("message:add", { message: input.value });
-//   input.value = "";
-// });
 export default socket;
--- lib/chat_app_web/plugs/set_user.ex
+++ lib/chat_app_web/plugs/set_user.ex
@@ -1,25 +1,27 @@
 defmodule ChatAppWeb.Plugs.SetUser do
-
   import Plug.Conn
   alias ChatApp.Repo
   alias ChatApp.Accounts.User
 
   def init(_params) do
-
   end
 
   def call(conn, _params) do
-      user_id = Plug.Conn.get_session(conn, :current_user_id)
-      cond do
-        current_user = user_id && Repo.get(User, user_id) ->
-          conn
-          |> assign(:current_user, current_user)
-          |> assign(:signed_in?, true)
+    user_id = Plug.Conn.get_session(conn, :current_user_id)
 
-          true ->
-            conn
-            |> assign(:current_user, nil)
-            |> assign(:signed_in?, false)
-      end
+    cond do
+      current_user = user_id && Repo.get(User, user_id) ->
+        token = Phoenix.Token.sign(conn, "user_token", user_id)
+
+        conn
+        |> assign(:current_user, current_user)
+        |> assign(:signed_in?, true)
+        |> assign(:user_token, token)
+
+      true ->
+        conn
+        |> assign(:current_user, nil)
+        |> assign(:signed_in?, false)
+    end
   end
 end
