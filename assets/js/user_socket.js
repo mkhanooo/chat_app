8dfeb884de0e7b3bad24d2036f878c98b8d7a7ee
mkhanooo <mkhan2khan@live.com>
2022-03-03 11:37:19 +0500

    presence and typing functions added

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -1,13 +1,14 @@
 import { Socket, Presence } from "phoenix";
 
-
 let socket = new Socket("/socket", { params: { token: window.userToken } });
 let roomId = window.roomId;
-let presences = {}
-const timeout = 3000;
+let presences = {};
+
 if (roomId) {
   socket.connect();
-
+  const timeout = 3000;
+  var typingTimer;
+  let userTyping = false;
   let channel = socket.channel(`room:${roomId}`, {});
   channel
     .join()
@@ -23,18 +24,17 @@ if (roomId) {
     displayMessage(message);
   });
 
-  channel.on("presence_state", state => {
-    presences = Presence.syncState(presences, state)
-    console.log(presences)
-    displayUsers(presences)
-  })
+  channel.on("presence_state", (state) => {
+    presences = Presence.syncState(presences, state);
+    console.log(presences);
+    displayUsers(presences);
+  });
 
-  channel.on("presence_diff", diff => {
-    presences = Presence.syncDiff(presences, diff)
-    console.log(presences)
-    displayUsers(presences)
-  })
-
+  channel.on("presence_diff", (diff) => {
+    presences = Presence.syncDiff(presences, diff);
+    console.log(presences);
+    displayUsers(presences);
+  });
 
   document.querySelector("#message-form").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -44,6 +44,30 @@ if (roomId) {
     });
     input.value = "";
   });
+  document.querySelector("#message-body").addEventListener("keydown", () => {
+    userStartsTyping();
+    clearTimeout(typingTimer);
+  });
+  document.querySelector("#message-body").addEventListener("keyup", () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(userStopTyping, timeout);
+  });
+  const userStartsTyping = () => {
+    if (userTyping) {
+      return;
+    }
+    userTyping = true;
+    channel.push("user:typing", {
+      typing: true,
+    });
+  };
+  const userStopTyping = () => {
+    clearTimeout(typingTimer);
+    userTyping = false;
+    channel.push("user:typing", {
+      typing: false,
+    });
+  };
   const displayMessage = (msg) => {
     let template = `
       <li class="list-group-item"><strong> ${msg.user.username}</strong>:${msg.body}</li>`;
@@ -54,10 +78,14 @@ if (roomId) {
     let usersOnline = Presence.list(
       presences,
       (_id, { metas: [user, ...rest] }) => {
+        var typingTemplate =''
+        if (user.typing) {
+          typingTemplate = '<i>(typing...)</i>';
+        }
         return `
-        <div id="user-${user.user_id}"><strong class="text-secondary">${user.username}</strong></div>`;
+        <div id="user-${user.user_id}"><strong class="text-secondary">${user.username}</strong>${typingTemplate}</div>`;
       }
-    ).join("")
+    ).join("");
     document.querySelector("#users-online").innerHTML = usersOnline;
   };
 }
